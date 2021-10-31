16803da791cdf06857288dbf78436e45ae32817d
aatracy7 <85047522+aatracy7@users.noreply.github.com>
2021-10-30 21:59:24 -0600

    Create find_so_called_passing_cars.py

---
+++ find_so_called_passing_cars.py
@@ -0,0 +1,17 @@
+def find_so_called_passing_cars(A):
+    #This finds the number of 1s that occur after a 0 in an array of 0 and 1s 
+    n = len(A)
+    prefix_sums = [0] * (n + 1)
+    east_cars = 0
+    already_passed = 0
+    for k in range(1, n+1):
+        prefix_sums[k] = prefix_sums[k-1] + A[k-1]
+    for i, x in enumerate(prefix_sums[1:]):
+        if x == prefix_sums[i]:
+            east_cars += 1
+            already_passed += x
+    total_passing_cars = east_cars * prefix_sums[-1] - already_passed
+    if total_passing_cars > 1000000000:
+        return -1
+    else:
+        return total_passing_cars
